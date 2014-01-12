fbdc334f973384e9c885978a1b26442cda416bb5
Helen Hood <helen.c.hood@gmail.com>
2014-01-12 12:22:29 -0500

    All guesses displayed on the page

--- js/app.js
+++ js/app.js
@@ -3,5 +3,7 @@ $("div.alert-box").text("Guess a number between 0 and " + randNum);
 
 $("#submit").click(function(value){
   var guess = $("input[type='text']").val();
+  $(".hidden").show();
+  $("#guesses").append("<li>"+guess+"</li>");
   console.log("Here's the guess: "+guess);
 });
