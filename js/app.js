5991237d075075eb47c8cfb3ac706c2aa96d837b
Helen Hood <helen.c.hood@gmail.com>
2014-01-12 13:05:08 -0500

    User story 3 complete - Feedback displayed on page

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 var randNum = Math.floor(Math.random()*101);
 var secretNum = Math.floor(Math.random()*randNum);
-var numGuesses = 0
+var numGuesses = 0;
 
 $("div.alert-box").text("Guess a number between 0 and " + randNum);
 
 $("#submit").click(function(value){
-  var guess = $("input[type='text']").val();
+  var guessString = $("input[type='text']").val();
+  var guess = parseInt(guessString, 10);
   numGuesses += 1;
-  if (guess === secretNum)
-    console.log("Congratulations, you've guessed the number in "+numGuesses+" guesses!");
-  else if (guess < secretNum)
-    console.log(guess+" was too low, try again...");
-  else if (guess > secretNum)
-    console.log(guess+" was too high, try again...");
-
   $(".hidden").show();
+  if (guess === secretNum)
+    $("#feedback").replaceWith("<p id='feedback'>Congratulations, you've guessed the number in "+numGuesses+" guesses!</p>");
+  else if (guess < secretNum)
+    $("#feedback").replaceWith("<p id='feedback'>"+guess+" was too low, try again...</p>");
+  else if (guess > secretNum)
+    $("#feedback").replaceWith("<p id='feedback'>"+guess+" was too high, try again...</p>");
+
   $("#guesses").append("<li>"+guess+"</li>");
 });
