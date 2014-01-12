6244aa8744ab1f2abc834b234a56d756d5957f24
Helen Hood <helen.c.hood@gmail.com>
2014-01-12 12:06:48 -0500

    User story 2 - Guess can be read; not yet compared to value

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
 var randNum = Math.floor(Math.random()*101);
 $("div.alert-box").text("Guess a number between 0 and " + randNum);
+
+$("#submit").click(function(value){
+  var guess = $("input[type='text']").val();
+  console.log("Here's the guess: "+guess);
+});
