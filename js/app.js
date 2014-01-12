f68c957bb12422e364b3af9d3344eb8a8f3693d4
Helen Hood <helen.c.hood@gmail.com>
2014-01-12 13:16:40 -0500

    New Game button appears upon winning

--- js/app.js
+++ js/app.js
@@ -2,16 +2,21 @@ var randNum = Math.floor(Math.random()*101);
 var secretNum = Math.floor(Math.random()*randNum);
 var numGuesses = 0;
 
+function showSuccessButton() {
+  $("a.success").show();
+}
+
 $("div.alert-box").text("Guess a number between 0 and " + randNum);
 
-$("#submit").click(function(value){
+$("a.submit").click(function(value){
   var guessString = $("input[type='text']").val();
   var guess = parseInt(guessString, 10);
   numGuesses += 1;
-  $(".hidden").show();
-  if (guess === secretNum)
+  $("p.hidden").show();
+  if (guess === secretNum){
     $("#feedback").replaceWith("<p id='feedback'>Congratulations, you've guessed the number in "+numGuesses+" guesses!</p>");
-  else if (guess < secretNum)
+    showSuccessButton();
+  } else if (guess < secretNum)
     $("#feedback").replaceWith("<p id='feedback'>"+guess+" was too low, try again...</p>");
   else if (guess > secretNum)
     $("#feedback").replaceWith("<p id='feedback'>"+guess+" was too high, try again...</p>");
